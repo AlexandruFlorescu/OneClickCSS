c13bc56c5224a84d8387e15d3f30536432ac14f0
verosik <veross4cs@gmail.com>
2020-03-14 16:24:10 +0200

    Clinic Detail Page

--- src/pages/clinicDetail/clinicDetail.component.ts
+++ src/pages/clinicDetail/clinicDetail.component.ts
@@ -7,12 +7,12 @@ import { Component } from '@angular/core';
 })
 export class ClinicDetailPageComponent {
   title = 'OneClickUI';
-  clinic = { 'Name': 'Clinica Mea', 
+  clinic = { 'Name': 'Clinica Mea',
             'Email': 'acdc152@gmail.com',
             'PhoneNumber': '0727318727',
             'Address':'Iuliu Maniu 28',
             'Rating': '4',
-            'Description': '    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc maximus, nulla ut commodo sagittis, sapien dui mattis dui, non pulvinar lorem felis nec erat',
+            'Description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc maximus, nulla ut commodo sagittis, sapien dui mattis dui, non pulvinar lorem felis nec erat',
             'Facebook': 'https://www.facebook.com/AlexMetalhead',
             'Instagram': 'https://www.instagram.com/flowerdts/',
             'Twitter': 'https://twitter.com/FlowerDTS',
